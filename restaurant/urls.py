2d25686feafb4253659fac3bab527ea07639255c
voytkojazz <75126304+voytkojazz@users.noreply.github.com>
2022-09-19 23:24:14 +0200

    Update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,7 +19,7 @@ from inventory import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),
+#     path('', include('inventory.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+#     path('accounts/', include('accounts.urls')),
 ]
